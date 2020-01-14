7a0ac53b0dad3075be6fb52481a1a871a57320c5
Ludmila Korchnoy <lkorchnoy@yahoo.com>
2020-01-14 06:46:46 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,39 @@ class Song
     self.class.all << self
   end
 
+  def Song.create
+   Song.self.all << self.new
+  end
+
+  def Song.new_by_name(name)
+    song = Song.new_by_name
+  end
+  
+  def Song.create_by_name(name)
+    song.name = name
+    @@all << song
+  end
+  
+  def Song.find_by_name(name)
+    @@all.find{ |song| song.name == name}
+  end
+
+   def Song.find_or_create_by_name(name)
+    name.each_with_index(1) do |song, index|
+         puts "#{index}.#{song}"
+    end
+    
+    def Song.alphabetical%w()
+      array.sort
+    end
+    
+    def Song.new_from_filename(name)
+      song.name.split ("").collect{|song| song.name << mp3}.join("")
+      artist_name = Taylor Swift
+    end
+
+   def self.destroy_all
+     self.all.clear
+   end
+   
 end
